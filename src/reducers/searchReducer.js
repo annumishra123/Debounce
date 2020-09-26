40f617b08eebd38192c498edd87fa38ead2a740b
annumishra123 <anurag786332@gmail.com>
2020-09-27 02:43:32 +0530

    commit

---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,42 @@
+import {
+      SEARCH_MOVIE,
+      FETCH_MOVIES,
+      FETCH_MOVIE,
+      LOADING
+  } from '../actions/types';
+  
+  const initialState = {
+    text: '',
+    movies: {
+        Search: []
+    },
+    loading: false,
+    movie: [],
+    page: 1
+  };
+  
+  export default function(state = initialState, action) {
+    switch (action.type) {
+      case SEARCH_MOVIE:
+        return {
+          ...state,
+          text: action.payload,
+          loading: false,
+          page: 1
+        };
+      case FETCH_MOVIES:
+        return {
+          ...state,
+          movies: {...state.movies, Search: [...state.movies.Search, ...action.payload.Search]},
+          loading: false,
+          page: state.page+1
+        };
+      case LOADING:
+        return {
+          ...state,
+          loading: true
+        };
+      default:
+        return state;
+    }
+  }
